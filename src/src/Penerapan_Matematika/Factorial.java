7db517bf79d55cde0cb6eaeb7ea73bb92338802e
gnptr <12050120370@students.uin-suska.ac.id>
2021-10-22 20:25:50 +0700

    Contoh Penerapan Rekursif

---
+++ src/src/Penerapan_Matematika/Factorial.java
@@ -0,0 +1,23 @@
+package Penerapan_Matematika;
+
+import java.util.Scanner;
+public class Factorial {
+    static int factorial (int n) {
+        if (n==1) {
+            return 1;
+        }
+        else
+            return n * factorial( n - 1);
+    }
+    public static void main(String[] args) {
+        int bil, hasil;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Masukkan Bilangan : ");
+        bil = scan.nextInt();
+        hasil = factorial(bil);
+
+        System.out.println(" Faktorial dari " + bil + "adalah : " + hasil);
+    }
+}
